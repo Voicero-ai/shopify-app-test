4f30b262a9f1644791e690b27011d83d21118720
David Fales <davidfalesct@gmail.com>
2025-05-17 21:48:35 -0600

    added proxy?

---
+++ app/routes/app.proxy.tsx
@@ -0,0 +1,25 @@
+import { ActionFunction } from "@remix-run/node";
+import { authenticate } from "app/shopify.server";
+import { Page } from "@shopify/polaris";
+
+
+export const action: ActionFunction = async ({ request }) => {
+    console.log("--------hit app proxy--------")
+
+    const session = await authenticate.public.appProxy(request);
+    if(session) {
+        console.log("session", session)
+    }
+
+    return null;
+}
+
+const Proxy = () => {
+    return (
+        <Page>
+         Proxy   
+        </Page>
+    )
+}
+
+export default Proxy;
